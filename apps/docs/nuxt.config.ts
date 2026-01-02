db9a09712b1f509c1e4c66e7fe8c11eb07d4e87e
Victor Damacena <victordamacenaalbino@gmail.com>
2026-01-02 15:52:03 -0400

    Remove all files from initial commit for project reset

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -1,10 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [
       tailwindcss(),
---
+++ apps/docs/tailwind.config.ts
@@ -0,0 +1,7 @@
+export default {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,vue}",
+        "./components/**/*.{js,ts,jsx,tsx,vue}",
+        "./registry/**/*.{js,ts,jsx,tsx,vue}"
+    ],
+}
